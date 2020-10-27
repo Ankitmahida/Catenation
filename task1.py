3885a1423a96fbeeec10ff9d35ac0e3f5351c2f2
Ankitmahida <72763514+Ankitmahida@users.noreply.github.com>
2020-10-27 15:04:09 -0400

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,48 @@
+# Exercise 1
+a=10,;b=10.20;c='Ankit'
+
+# Exercise 2
+a=complex(2,4)
+print(type(a))
+b=15
+a=b
+print(type(a))
+
+#Exercise 3
+a=10,b=90
+Result=a
+a=b
+y=Result
+Print(a)
+Print(b)
+
+# Exercise 4
+#python version 2
+x=raw_input("Enter the value")
+print x
+
+#python version3x
+input1=eval(input("Enter the value"))
+print(x)
+
+# Exercise 5
+a, b = input("Enter two numbers between 1-10 : ").split() 
+print("first value:",a)
+print("second value:",b)
+
+
+z= int(a) + int(b)
+print(result)
+
+
+# Exercise 6
+x=input("Enter the number:")
+print("The value of number is :",x)
+
+# Exercise 7
+a='AnkitMahida'
+b='ankitmahida'
+c='ANKITMAHIDA'
+
+# Exercise 8
+ # the value will be changed because python take recent value if have an option for variable. 
